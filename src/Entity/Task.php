fd5c9f91d010e7c8553a1d0474662fcb84686799
Tac Tacelosky <tacman@gmail.com>
2022-10-24 07:51:27 -0400

    get demo working with bundle

--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -7,8 +7,9 @@ use App\Repository\TaskRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Survos\WorkflowBundle\Traits\MarkingInterface;
 use Survos\WorkflowBundle\Traits\MarkingTrait;
-use Survos\WorkflowHelperBundle\Attribute\Workflow;
-use Survos\WorkflowHelperBundle\Attribute\Transition;
+use Survos\WorkflowBundle\Attribute\Workflow;
+use Survos\WorkflowBundle\Attribute\Transition;
+use Survos\WorkflowBundle\Attribute\Place;
 
 #[ORM\Entity(repositoryClass: TaskRepository::class)]
 #[Workflow('PLACE_')]
@@ -19,12 +20,14 @@ class Task implements MarkingInterface
     const PLACE_DRAFT = 'draft';
     const PLACE_REVIEWED = 'reviewed';
     const PLACE_REJECTED = 'rejected';
+    #[Place(metadata: ['description' => "Published on the website"])]
     const PLACE_PUBLISHED = 'published';
 
-    #[Transition(self::PLACE_DRAFT, self::PLACE_REVIEWED)]
+    #[Transition(self::PLACE_DRAFT, self::PLACE_REVIEWED, metadata: ['description' => 'When the draft is ready'])]
     const TRANSITION_REVIEW = 'review';
 
-    #[Transition(self::PLACE_REVIEWED, self::PLACE_PUBLISHED)]
+    #[Transition(from: self::PLACE_REVIEWED, to: self::PLACE_PUBLISHED,
+        guard: "is_granted('ROLE_REVIEWER')")]
     const TRANSITION_PUBLISH = 'publish';
 
     #[Transition([self::PLACE_REVIEWED, self::PLACE_DRAFT], self::PLACE_REJECTED)]
