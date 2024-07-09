01d46c127d757aaa1bceda7a781efc9bf0609cfe
Tac Tacelosky <tacman@gmail.com>
2024-07-09 05:26:39 -0400

    use new workflow bundle

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -4,21 +4,25 @@ namespace App\Controller;
 
 use Survos\WorkflowBundle\Service\WorkflowHelperService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\AutowireLocator;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\DependencyInjection\ServiceLocator;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class HomepageController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function index(WorkflowHelperService $workflowHelperService, ParameterBagInterface $parameters)
+    public function index(WorkflowHelperService $workflowHelperService,
+                          #[AutowireLocator('workflow.state_machine')] ServiceLocator $workflowsByTaggedIterator,
+    ): Response
     {
         $workflows = $workflowHelperService->getWorkflowsGroupedByClass();
         $configuration =  $workflowHelperService->getWorkflowConfiguration();
 
         return $this->render('homepage/index.html.twig', [
             'workflows' => $workflows,
-            'workflowsByTaggedIterator' => $workflowHelperService->getWorkflowsFromTaggedIterator(),
+            'workflowsByTaggedIterator' => $workflowsByTaggedIterator,// $workflowHelperService->getWorkflowsFromTaggedIterator(),
             'workflowsConfiguration' => $configuration,
         ]);
     }
