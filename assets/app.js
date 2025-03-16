1ec02323a171da35f3f6860c8bb72a70b2e51d9c
Tac Tacelosky <tacman@gmail.com>
2025-03-16 12:33:56 -0400

    update libraries

--- assets/app.js
+++ assets/app.js
@@ -3,8 +3,11 @@ console.log('Loading stimulus and bootstrap')
 import './styles/app.css';
 
 import './bootstrap.js'; // stimulus
-import 'bootstrap'; // from getbootstrap
-import 'bootstrap/dist/css/bootstrap.min.css' ;
+// import 'bootstrap'; // from getbootstrap
+// import 'bootstrap/dist/css/bootstrap.min.css' ;
 
 // import '@picocss/pico';
 // import '@picocss/pico/css/pico.min.css';
+
+import '@tabler/core';
+import '@tabler/core/dist/css/tabler.min.css';
--- importmap.php
+++ importmap.php
@@ -39,4 +39,119 @@ return [
         'version' => '2.0.6',
         'type' => 'css',
     ],
+    '@tabler/core' => [
+        'version' => '1.0.0-beta21',
+    ],
+    '@tabler/core/dist/css/tabler.min.css' => [
+        'version' => '1.0.0-beta21',
+        'type' => 'css',
+    ],
+    'd3' => [
+        'version' => '7.9.0',
+    ],
+    'd3-array' => [
+        'version' => '3.2.4',
+    ],
+    'd3-axis' => [
+        'version' => '3.0.0',
+    ],
+    'd3-brush' => [
+        'version' => '3.0.0',
+    ],
+    'd3-chord' => [
+        'version' => '3.0.1',
+    ],
+    'd3-color' => [
+        'version' => '3.1.0',
+    ],
+    'd3-contour' => [
+        'version' => '4.0.2',
+    ],
+    'd3-delaunay' => [
+        'version' => '6.0.4',
+    ],
+    'd3-dispatch' => [
+        'version' => '3.0.1',
+    ],
+    'd3-drag' => [
+        'version' => '3.0.0',
+    ],
+    'd3-dsv' => [
+        'version' => '3.0.1',
+    ],
+    'd3-ease' => [
+        'version' => '3.0.1',
+    ],
+    'd3-fetch' => [
+        'version' => '3.0.1',
+    ],
+    'd3-force' => [
+        'version' => '3.0.0',
+    ],
+    'd3-format' => [
+        'version' => '3.1.0',
+    ],
+    'd3-geo' => [
+        'version' => '3.1.1',
+    ],
+    'd3-hierarchy' => [
+        'version' => '3.1.2',
+    ],
+    'd3-interpolate' => [
+        'version' => '3.0.1',
+    ],
+    'd3-path' => [
+        'version' => '3.1.0',
+    ],
+    'd3-polygon' => [
+        'version' => '3.0.1',
+    ],
+    'd3-quadtree' => [
+        'version' => '3.0.1',
+    ],
+    'd3-random' => [
+        'version' => '3.0.1',
+    ],
+    'd3-scale' => [
+        'version' => '4.0.2',
+    ],
+    'd3-scale-chromatic' => [
+        'version' => '3.1.0',
+    ],
+    'd3-selection' => [
+        'version' => '3.0.0',
+    ],
+    'd3-shape' => [
+        'version' => '3.2.0',
+    ],
+    'd3-time' => [
+        'version' => '3.1.0',
+    ],
+    'd3-time-format' => [
+        'version' => '4.1.0',
+    ],
+    'd3-timer' => [
+        'version' => '3.0.1',
+    ],
+    'd3-transition' => [
+        'version' => '3.0.1',
+    ],
+    'd3-zoom' => [
+        'version' => '3.0.0',
+    ],
+    'internmap' => [
+        'version' => '2.0.3',
+    ],
+    'delaunator' => [
+        'version' => '5.0.0',
+    ],
+    'robust-predicates' => [
+        'version' => '3.0.2',
+    ],
+    'd3-graphviz' => [
+        'version' => '5.6.0',
+    ],
+    '@hpcc-js/wasm/graphviz' => [
+        'version' => '2.22.3',
+    ],
 ];
